0862e263d6fb03841f2ca5d47af76b9a2bb26175
yamilelias <yamileliassoto@gmail.com>
2017-03-18 14:30:40 -0700

    Annotations & Contacts Controllers Added

---
+++ CRM/Controllers/AnnotationsController.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CRM.Models;
+
+namespace CRM.Controllers
+{
+    public class AnnotationsController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public AnnotationsController(DatabaseContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Annotations
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Annotations.ToListAsync());
+        }
+
+        // GET: Annotations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var annotations = await _context.Annotations
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (annotations == null)
+            {
+                return NotFound();
+            }
+
+            return View(annotations);
+        }
+
+        // GET: Annotations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Annotations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Date,Description")] Annotations annotations)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(annotations);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(annotations);
+        }
+
+        // GET: Annotations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var annotations = await _context.Annotations.SingleOrDefaultAsync(m => m.Id == id);
+            if (annotations == null)
+            {
+                return NotFound();
+            }
+            return View(annotations);
+        }
+
+        // POST: Annotations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Date,Description")] Annotations annotations)
+        {
+            if (id != annotations.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(annotations);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnnotationsExists(annotations.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(annotations);
+        }
+
+        // GET: Annotations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var annotations = await _context.Annotations
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (annotations == null)
+            {
+                return NotFound();
+            }
+
+            return View(annotations);
+        }
+
+        // POST: Annotations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var annotations = await _context.Annotations.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Annotations.Remove(annotations);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool AnnotationsExists(int id)
+        {
+            return _context.Annotations.Any(e => e.Id == id);
+        }
+    }
+}
